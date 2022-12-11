34d68cb2459e207a5f50dd94f962d214212ec732
Dmytro Voronov <dimolg22@gmail.com>
2022-12-11 15:02:36 +0200

    add react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Container} from 'react-bootstrap'
+import HomeScreen from "./screens/HomeScreen";
+import {Route, Routes} from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 
 
 function App() {
   return (
-    <>
+      <>
      <Header/>
         <main className={'py-3'}>
             <Container>
-                <h1>Welcome To ProShop</h1>
+                <Routes>
+                <Route path={'/'} element={<HomeScreen/>}/>
+                <Route path={'/product/:id'} element={<ProductScreen/>}/>
+                </Routes>
             </Container>
-
         </main>
      <Footer/>
-    </>
+      </>
   );
 }
 
